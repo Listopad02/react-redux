ad3335fcd3114a627e29c5d081c1fc4072043b1d
Listopad02 <123ayetvbass@gmail.com>
2021-11-29 20:50:55 +0300

    create-react-app config rebuilt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
+    })
+  }
+
+  render() {
+    return (
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+
+        <hr/>
+
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
