ae2c0ac875648971a729d34f363e6d6c101a4181
Listopad02 <123ayetvbass@gmail.com>
2021-11-29 21:37:09 +0300

    react-redux library added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './redux/rootReducer';
+
+const store = createStore(rootReducer);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 reportWebVitals();
