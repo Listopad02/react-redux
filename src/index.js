32a57da3d96ca82b0f57bd2457930ec520e308ec
Listopad02 <123ayetvbass@gmail.com>
2021-12-01 22:22:00 +0300

    middleware function added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './redux/rootReducer';
 
-const store = createStore(rootReducer);
+// function loggerMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+//       const result = next(action);
+//       console.log('Middleware', store.getState());
+
+//       return result;
+//     }
+//   }
+// }
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action);
+  console.log('Middleware', store.getState());
+  return result;
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
   <Provider store={store}>
